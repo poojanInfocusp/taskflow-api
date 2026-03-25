2d2f40603054eacdfe2063f30f7641a92cd57687
Poojan <poojan@infocusp.com>
2026-03-25 17:12:35 +0530

    refactor(db): configure async engine pooling and async sessionmaker

--- app/db/session.py
+++ app/db/session.py
@@ -1,31 +1,17 @@
 from sqlalchemy.ext.asyncio import (AsyncSession, AsyncEngine, create_async_engine, async_sessionmaker)
-from sqlalchemy.orm import DeclarativeBase
-from typing import AsyncGenerator
 from app.config import settings
 
 engine : AsyncEngine = create_async_engine(
     settings.database_url,
-    echo=False,
     pool_size=10,
     max_overflow=10,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    pool_recycle=3600
 )
 
-AsyncSessionLocale = async_sessionmaker(
+AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False,
-    autocommit=False,
     autoflush=False,
 )
-
-async def get_db()->AsyncGenerator[AsyncSession,None]:
-    async with AsyncSessionLocale() as session:
-        try:
-            yield session
-        except Exception:
-            await session.rollback()
-            raise
-    
-class Base(DeclarativeBase):
-    pass
