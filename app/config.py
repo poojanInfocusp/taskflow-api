5e2487f47cae22521348be61d25c33b50a8cbf9d
Poojan <poojan@infocusp.com>
2026-03-26 23:22:29 +0530

    chore(config): update settings and dependency lockfile

--- app/config.py
+++ app/config.py
@@ -2,6 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url : str = ""
+    ACCESS_TOKEN_SECRET : str = ""
+    REFRESH_TOKEN_SECRET : str = ""
+    
     model_config = SettingsConfigDict(env_file=".env",case_sensitive=False)
 
 settings = Settings()
